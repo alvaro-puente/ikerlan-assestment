e506e0d063e76d78799dd3b284591836f264aa2c
Alvaro Puente Escudero <alvaro.puente-escudero@bmw.de>
2024-05-21 11:05:09 +0200

    MQTT SUbscriber is now more decoupled from processing knowledge

--- src/edge_server/MQTTSubscriber.py
+++ src/edge_server/MQTTSubscriber.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-from ProcessedDataDatabase import ProcessedDataDatabase
-from SensorsDataDatabase import SensorsDataDatabase
-from concurrent.futures import ThreadPoolExecutor
 from CustomLogger import CustomLogger
 import paho.mqtt.client as mqtt
 from Processor import Processor
 import json
-import uuid
 
 subscribersLogger = CustomLogger('/edge_server/logs/subscribers.log', "subscribers")
 
+"""
+This class provides all the methods to generate a MQTT subscriber that listens to a certain topic
+and process it in the way is defined in the processMessage() method from Processor class. 
+"""
 class MQTTSubscriber():
     
     def __init__(self, brokerAddress, brokerPort, user, password, clientID):
@@ -20,11 +20,6 @@ class MQTTSubscriber():
         self.brokerAddress = brokerAddress
         self.brokerPort = brokerPort
         self.clientID = clientID
-        # Connect to databases
-        self.sensorsDataDatabase = SensorsDataDatabase()
-        self.sensorsDataDatabase.connect()
-        self.processedDataDatabase = ProcessedDataDatabase()
-        self.processedDataDatabase.connect()
         # Processing functions
         self.processor = Processor()
         # Callbacks
@@ -59,44 +54,6 @@ class MQTTSubscriber():
         try:
             subscribersLogger.logger.debug(self.clientID + " received a message " + str(msg.payload))
             message = json.loads(msg.payload)
-            self.processMessage(message)
+            self.processor.processMessage(message)
         except Exception as e:
             subscribersLogger.logger.error(self.clientID + ": Error processing message: " + str(e))
-
-    # Store message in database
-    def processMessage(self, message):
-        # Store message
-        self.storeMessage(message)
-        # Check if database reaches more than numberOfEntries
-        numberOfEntries = 50
-        hasNumberEntries = self.sensorsDataDatabase.hasNumberOfEntries(numberOfEntries)
-        # If reaches...
-        if hasNumberEntries:
-            # Process data entries
-            self.applyProcessing(numberOfEntries)
-
-    # Store message in database
-    def storeMessage(self, message):
-        id = str(uuid.uuid4())
-        name = message["sensor_id"]
-        type = message["sensor_type"]
-        value = message["value"]
-        timestamp = message["timestamp"]
-        self.sensorsDataDatabase.addEntry(id, name, type, value, timestamp)
-        
-    # Process the message
-    def applyProcessing(self, numberOfEntries):
-        # Get numberOfentries number of values to be processed
-        idValuePairs = self.sensorsDataDatabase.obtainNumberOfEntriesValues(numberOfEntries)
-        ids = [row[0] for row in idValuePairs]
-        values = [row[1] for row in idValuePairs]
-        # Process values and store them in a new database
-        processedValues = self.processor.applyOutliersFilter(values)
-        for value in processedValues:
-            id = str(uuid.uuid4())
-            self.processedDataDatabase.addEntry(id, value)
-        # Delete processed values from database
-        self.sensorsDataDatabase.deleteEntriesById(ids)
-
-
-
--- src/edge_server/Processor.py
+++ src/edge_server/Processor.py
@@ -1,15 +1,60 @@
 #!/usr/bin/env python3
 
+from ProcessedDataDatabase import ProcessedDataDatabase
+from SensorsDataDatabase import SensorsDataDatabase
 from CustomLogger import CustomLogger
 import numpy as np
+import uuid
 
 processorLogger = CustomLogger('/edge_server/logs/processor.log', "processor")
 
 """
-This class provides all the relevant methods to filter data
+This class provides all the relevant methods to process data
 """
 class Processor():
-    
+
+    def __init__(self):
+        # Connect to databases
+        self.sensorsDataDatabase = SensorsDataDatabase()
+        self.sensorsDataDatabase.connect()
+        self.processedDataDatabase = ProcessedDataDatabase()
+        self.processedDataDatabase.connect()
+
+    # Store message in database
+    def processMessage(self, message):
+        # Store message
+        self.storeMessage(message)
+        # Check if database reaches more than numberOfEntries
+        numberOfEntries = 50
+        hasNumberEntries = self.sensorsDataDatabase.hasNumberOfEntries(numberOfEntries)
+        # If reaches...
+        if hasNumberEntries:
+            # Process data entries
+            self.applyProcessing(numberOfEntries)
+
+    # Store message in database
+    def storeMessage(self, message):
+        id = str(uuid.uuid4())
+        name = message["sensor_id"]
+        type = message["sensor_type"]
+        value = message["value"]
+        timestamp = message["timestamp"]
+        self.sensorsDataDatabase.addEntry(id, name, type, value, timestamp)
+        
+    # Process the message
+    def applyProcessing(self, numberOfEntries):
+        # Get numberOfentries number of values to be processed
+        idValuePairs = self.sensorsDataDatabase.obtainNumberOfEntriesValues(numberOfEntries)
+        ids = [row[0] for row in idValuePairs]
+        values = [row[1] for row in idValuePairs]
+        # Process values and store them in a new database
+        processedValues = self.applyOutliersFilter(values)
+        for value in processedValues:
+            id = str(uuid.uuid4())
+            self.processedDataDatabase.addEntry(id, value)
+        # Delete processed values from database
+        self.sensorsDataDatabase.deleteEntriesById(ids)
+
     # Outlier filters
     def applyOutliersFilter(self, data):
         try:
